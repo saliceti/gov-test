5d6e97cfa74ed25e298a2abd30e8a36bfd86b760
Colin Saliceti <c.saliceti@gmail.com>
2015-02-22 16:29:00 +0000

    Nginx starting ok

--- puppet/modules/gov-test/manifests/init.pp
+++ puppet/modules/gov-test/manifests/init.pp
@@ -5,4 +5,15 @@ class gov-test {
 	notify{"i'm in gov-test puppet":}
 
 	class { 'nginx': }
+
+	nginx::resource::vhost { 'test.gov':
+		proxy	=> 'http://be.test.gov'
+	}
+
+	nginx::resource::upstream { 'be.test.gov':
+		members => [
+			'localhost:3000',
+			'localhost:3001',
+		],
+	}
 }
